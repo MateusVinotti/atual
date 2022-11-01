3999e5408cd4aa35f8b38ba51d8a9ae8c00b728e
Vinotti <mateusvinotti@gmail.com>
2022-10-31 22:00:49 -0300

    first commit

---
+++ src/main/java/V/mateus/demo/Cliente.java
@@ -0,0 +1,37 @@
+package V.mateus.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+
+public class Cliente {
+
+    @Id
+    private Long id;
+    private String cpf;
+    private String nome;
+
+    public Cliente(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
